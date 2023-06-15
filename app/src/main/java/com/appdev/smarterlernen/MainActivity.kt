189f3fe829e7277c19636fec2c4c8140ef814e66
Laura_LL <lauralik@edu.aau.at>
2023-06-15 11:43:17 +0200

    Hinzufügen der Hauptseite mit Tab Layout

--- app/src/main/java/com/appdev/smarterlernen/MainActivity.kt
+++ app/src/main/java/com/appdev/smarterlernen/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.appdev.smarterlernen
 
+import PagerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.appdev.smarterlernen.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //initial binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tabLayout=binding.tabLayout
+        viewPager=binding.viewPager
+
+        val adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(StackOverview(), "Stapelübersicht")
+        adapter.addFragment(AddCards(), "Karten Hinzufügen")
+        adapter.addFragment(CardManagement(), "Verwaltung")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+
     }
 }
---
+++ app/src/main/java/com/appdev/smarterlernen/PagerAdapter.kt
@@ -0,0 +1,26 @@
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val fragmentList = ArrayList<Fragment>()
+    private val titleList = ArrayList<String>()
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titleList[position]
+    }
+}
