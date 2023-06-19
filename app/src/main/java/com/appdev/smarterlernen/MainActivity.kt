eef8551830e94addbbda27fb48935e3baa7224bd
Dominik Hartl <sierra.flails_01@icloud.com>
2023-06-19 23:02:07 +0200

    FAB implementiert

--- app/src/main/java/com/appdev/smarterlernen/MainActivity.kt
+++ app/src/main/java/com/appdev/smarterlernen/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.appdev.smarterlernen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager.widget.ViewPager
 import com.appdev.smarterlernen.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
@@ -21,11 +23,45 @@ class MainActivity : AppCompatActivity() {
         tabLayout=binding.tabLayout
         viewPager=binding.viewPager
 
+        val fabAdd = binding.fabAdd
+        val fabAddCard = binding.fabAddCard
+        val fabAddStack = binding.fabAddStack
+        val txtFabCard = binding.addCardActionText
+        val txtFabStack = binding.addStackActionText
+
+        var isAllFabsVisible = false
+
         val adapter = PagerAdapter(supportFragmentManager)
         adapter.addFragment(StackOverview(), "Stapel")
         adapter.addFragment(CardManagement(), "Verwaltung")
         viewPager.adapter = adapter
         tabLayout.setupWithViewPager(viewPager)
+
+        fabAdd.setOnClickListener{
+            if(!isAllFabsVisible) {
+                fabAddCard.show()
+                fabAddStack.show()
+                txtFabCard.visibility = View.VISIBLE
+                txtFabStack.visibility = View.VISIBLE
+                isAllFabsVisible = true
+            } else {
+                fabAddCard.hide()
+                fabAddStack.hide()
+                txtFabCard.visibility = View.INVISIBLE
+                txtFabStack.visibility = View.INVISIBLE
+                isAllFabsVisible = false
+            }
+        }
+
+        fabAddCard.setOnClickListener{
+            val intent = Intent(this, AddCardActivity::class.java)
+            startActivity(intent)
+        }
+
+        fabAddStack.setOnClickListener{
+            val intent = Intent(this, AddStackActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
