a1ade5a32cf2124ea8e9be1d60e43d73101fee22
Dominik Hartl <sierra.flails_01@icloud.com>
2023-07-05 20:19:19 +0200

    Stack hinzufügen eingebaut

--- app/src/main/java/com/appdev/smarterlernen/AddCardActivity.kt
+++ app/src/main/java/com/appdev/smarterlernen/AddCardActivity.kt
@@ -2,10 +2,21 @@ package com.appdev.smarterlernen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.appdev.smarterlernen.databinding.ActivityAddCardBinding
+import com.appdev.smarterlernen.databinding.ActivityMainBinding
 
 class AddCardActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddCardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
+
+        binding = ActivityAddCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.aaCardButton.setOnClickListener {
+
+        }
     }
 }
