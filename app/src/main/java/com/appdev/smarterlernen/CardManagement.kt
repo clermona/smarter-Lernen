d2b694b753c2fbd90fee752c9c3c1ecd4dd4e747
verenarath <verenarath@edu.aau.at>
2023-07-17 20:42:28 +0200

    still lauras name

--- app/src/main/java/com/appdev/smarterlernen/CardManagement.kt
+++ app/src/main/java/com/appdev/smarterlernen/CardManagement.kt
@@ -1,22 +1,75 @@
-package com.appdev.smarterlernen
+package com.gtappdevelopers.kotlingfgproject
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.appdev.smarterlernen.R
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
-class CardManagement : Fragment() {
+class MainActivity : AppCompatActivity() {
+
+    // on below line we are creating
+    // variables for our bar chart
+    lateinit var barChart: BarChart
+
+    // on below line we are creating
+    // a variable for bar data
+    lateinit var barData: BarData
+
+    // on below line we are creating a
+    // variable for bar data set
+    lateinit var barDataSet: BarDataSet
+
+    // on below line we are creating array list for bar data
+    lateinit var barEntriesList: ArrayList<BarEntry>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_card_management)
 
+        // on below line we are initializing
+        // our variable with their ids.
+        barChart = findViewById(R.id.idBarChart)
+
+        // on below line we are calling get bar
+        // chart data to add data to our array list
+        getBarChartData()
+
+        // on below line we are initializing our bar data set
+        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
+
+        // on below line we are initializing our bar data
+        barData = BarData(barDataSet)
+
+        // on below line we are setting data to our bar chart
+        barChart.data = barData
+
+        // on below line we are setting colors for our bar chart text
+        barDataSet.valueTextColor = Color.BLACK
+
+        // on below line we are setting color for our bar data set
+        barDataSet.setColor(resources.getColor(R.color.pink))
+
+        // on below line we are setting text size
+        barDataSet.valueTextSize = 16f
+
+        // on below line we are enabling description as false
+        barChart.description.isEnabled = false
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_management, container, false)
-    }
+    private fun getBarChartData() {
+        barEntriesList = ArrayList()
 
+        // on below line we are adding data
+        // to our bar entries list
+        barEntriesList.add(BarEntry(1f, 1f))
+        barEntriesList.add(BarEntry(2f, 2f))
+        barEntriesList.add(BarEntry(3f, 3f))
+        barEntriesList.add(BarEntry(4f, 4f))
+        barEntriesList.add(BarEntry(5f, 5f))
+
+    }
 }
