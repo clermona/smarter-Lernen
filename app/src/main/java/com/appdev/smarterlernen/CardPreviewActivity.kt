3b4b43d4c989a16eea9764f1fa03244f119059b1
Laura_LL <lauralik@edu.aau.at>
2023-07-22 13:52:31 +0200

    Karten koennen im Karten-Vorschau nun bearbeitet und geloescht werden

--- app/src/main/java/com/appdev/smarterlernen/CardPreviewActivity.kt
+++ app/src/main/java/com/appdev/smarterlernen/CardPreviewActivity.kt
@@ -2,7 +2,9 @@ package com.appdev.smarterlernen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.MenuItem
+import android.widget.Toast
 import com.appdev.smarterlernen.database.AppDatabase
 import com.appdev.smarterlernen.database.entities.Card
 import com.appdev.smarterlernen.database.entities.Stack
@@ -10,8 +12,10 @@ import com.appdev.smarterlernen.database.interfaces.CardDao
 import com.appdev.smarterlernen.database.interfaces.StackDao
 import com.appdev.smarterlernen.databinding.ActivityCardPreviewBinding
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class CardPreviewActivity : AppCompatActivity() {
 
@@ -22,7 +26,6 @@ class CardPreviewActivity : AppCompatActivity() {
     lateinit var stack: Stack
     lateinit var cardList: List<Card>
     var tabIndex: Int = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +41,9 @@ class CardPreviewActivity : AppCompatActivity() {
 
         tabIndex = 0
 
+        binding.txtFront.isEnabled = true
+        binding.txtBack.isEnabled = true
+
         val stackId = intent.getIntExtra("stack_id", 0)
 
         runBlocking {
@@ -49,33 +55,73 @@ class CardPreviewActivity : AppCompatActivity() {
 
         binding.stackName.text = stack.title
 
-        if(cardList.isNotEmpty()) {
+        if (cardList.isNotEmpty()) {
             updateCard(false)
 
             binding.pbCardProgress.max = cardList.size
-            if(tabIndex == 0) {
+            if (tabIndex == 0) {
                 binding.pbCardProgress.progress = 1
             } else {
                 binding.pbCardProgress.progress = tabIndex - 1
+            }
+
+            binding.btnSave.setOnClickListener {
+
+                runBlocking {
+                    saveCard()
+                }
+
+            }
+            binding.btnDelete.setOnClickListener {
+                if (cardList.isNotEmpty() && tabIndex >= 0 && tabIndex < cardList.size) {
+                    val cardToDelete = cardList[tabIndex]
+
+                    //Loeschoperation in einer Coroutine auf einem Hintergrundthread aus
+                    GlobalScope.launch(Dispatchers.IO) {
+                        cardDao.delete(cardToDelete)
+                    }
+
+                    cardList = cardList.toMutableList().apply { removeAt(tabIndex) }
+                    updateCard(false) // Um die Anzeige zu aktualisieren
+                }
             }
 
             binding.btnNext.setOnClickListener { updateCard(true) }
 
             binding.btnPrevious.setOnClickListener { updateCard(true, up = false) }
         }
+
     }
+
+    private suspend fun saveCard() {
+        val frontText = binding.txtFront.text.toString()
+        val backText = binding.txtBack.text.toString()
+
+        // Überpruefung, ob es eine gueltige Karte gibt, bevor speichern
+        if (tabIndex >= 0 && tabIndex < cardList.size) {
+            val cardToUpdate = cardList[tabIndex].copy(frontSide = frontText, backSide = backText)
+
+            // Die Aktualisierungsoperation in einem Hintergrundthread ausführen
+            withContext(Dispatchers.IO) {
+                cardDao.update(cardToUpdate)
+            }
+        }
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 onBackPressed()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     private fun updateCard(changeIndex: Boolean = true, up: Boolean = true) {
-        if(changeIndex) {
+        if (changeIndex) {
             if (up) {
                 if (tabIndex < cardList.size - 1) {
                     tabIndex++
@@ -90,13 +136,27 @@ class CardPreviewActivity : AppCompatActivity() {
         binding.btnPrevious.isEnabled = tabIndex != 0
         binding.btnNext.isEnabled = (tabIndex < cardList.size - 1)
 
-        binding.txtFront.text = cardList[tabIndex].frontSide
-        binding.txtBack.text = cardList[tabIndex].backSide
-
         binding.pbCardProgress.progress = tabIndex + 1
 
         val cardNumberString = getString(R.string.label_card_number, tabIndex + 1, cardList.size)
         binding.cardNumber.text = cardNumberString
+
+        // Daten aus der Datenbank neu abrufen
+        runBlocking {
+            launch(Dispatchers.Default) {
+                cardList = cardDao.getByStackId(stack.id)
+            }
+        }
+
+        // Die TextViews mit den Werten der aktuellen Karte aus der Liste aktualisieren
+        val frontSide = cardList[tabIndex].frontSide
+        val backSide = cardList[tabIndex].backSide
+
+        // Pruefen, ob die Werte nicht null sind, bevor sie in den TextViews gesetzt werden
+        if (frontSide != null && backSide != null) {
+            binding.txtFront.text = Editable.Factory.getInstance().newEditable(frontSide)
+            binding.txtBack.text = Editable.Factory.getInstance().newEditable(backSide)
+        }
     }
 
 }
