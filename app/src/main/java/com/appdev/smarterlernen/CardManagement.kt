01ab3911f580d73af3781795ef7d1c0546d56d1f
verenarath <verenarath@edu.aau.at>
2023-07-28 11:07:10 +0200

    design adjustments

--- app/src/main/java/com/appdev/smarterlernen/CardManagement.kt
+++ app/src/main/java/com/appdev/smarterlernen/CardManagement.kt
@@ -1,120 +1,3 @@
-//import android.graphics.Color
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import androidx.fragment.app.Fragment
-//import com.appdev.smarterlernen.R
-//import com.appdev.smarterlernen.database.AppDatabase
-//import com.appdev.smarterlernen.database.entities.Card
-//import com.appdev.smarterlernen.database.interfaces.CardDao
-//import com.github.mikephil.charting.charts.BarChart
-//import com.github.mikephil.charting.components.XAxis
-//import com.github.mikephil.charting.data.BarData
-//import com.github.mikephil.charting.data.BarDataSet
-//import com.github.mikephil.charting.data.BarEntry
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.launch
-//import kotlinx.coroutines.runBlocking
-//
-//class CardManagement : Fragment() {
-//
-//    // Variablen fuer database
-//    lateinit var database: AppDatabase
-//    lateinit var cardDao: CardDao
-//    lateinit var cardList: List<Card>
-//    val entries = mutableListOf<BarEntry>()
-//    var countEasyCards = 0
-//    var countMediumCards = 0
-//    var countHardCards = 0
-//    lateinit var barChart: BarChart
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val rootView = inflater.inflate(R.layout.fragment_card_management, container, false)
-//
-//        barChart = rootView.findViewById(R.id.barChart)
-//
-//        database = AppDatabase.getInstance(requireContext())
-//        cardDao = database.cardDao()
-//
-//        // Load the initial countEasyCards value and add the entry to the list
-//        runBlocking {
-//            launch(Dispatchers.Default) {
-//                countEasyCards = cardDao.getCountEasyCards()
-//                countMediumCards = cardDao.getCountMediumCards();
-//                countHardCards = cardDao.getCountHardCards()
-//
-//                entries.clear()
-//                entries.add(BarEntry(0f, countEasyCards.toFloat()))
-//                entries.add(BarEntry(1f, countMediumCards.toFloat()))
-//                entries.add(BarEntry(2f, countMediumCards.toFloat()))
-//                updateChartData()
-//            }
-//        }
-//
-//        // Customize the BarDataSet
-//        val dataSet = BarDataSet(entries, "leicht") // "Data" is the label for the dataset
-//        dataSet.color = Color.rgb(48,103,84) // Set the color of the bars
-//
-//        // Create BarData and assign the BarDataSet to it
-//        val data = BarData(dataSet)
-//
-//        val xAxis = barChart.xAxis
-//        xAxis.position = XAxis.XAxisPosition.BOTTOM // Set the position of the x-axis
-//        xAxis.setDrawLabels(true) // Enable the display of x-axis labels
-//        xAxis.textColor = Color.BLACK // Set the color of the x-axis labels
-//        xAxis.textSize = 12f // Set the text size of the x-axis labels
-//        xAxis.granularity = 1f
-//
-//        // Customize the appearance of the chart
-//        barChart.setFitBars(true) // Make the bars fit the available space
-//        barChart.description.isEnabled = false // Disable the chart description
-//        barChart.data = data // Assign the BarData to the chart
-//
-//        // Refresh the chart to display the data
-//        barChart.invalidate()
-//
-//        return rootView
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//
-//        runBlocking {
-//            launch(Dispatchers.Default) {
-//                countEasyCards = cardDao.getCountEasyCards()
-//                entries.clear()
-//                entries.add(BarEntry(0f, countEasyCards.toFloat()))
-//                updateChartData()
-//            }
-//        }
-//
-//        barChart.invalidate()
-//    }
-//
-//    private fun updateChartData() {
-//        // Check if barChart.data is not null
-//        val barData = barChart.data ?: BarData()
-//        // Check if the BarDataSet exists or create a new one
-//        val dataSet: BarDataSet = if (barData.dataSetCount > 0) {
-//            barData.getDataSetByIndex(0) as BarDataSet
-//        } else {
-//            BarDataSet(entries, "Schwierigkeit")
-//        }
-//        dataSet.values = entries
-//        dataSet.color = Color.GREEN // Set the color of the bars
-//        // Add the dataset to the BarData
-//        barData.addDataSet(dataSet)
-//        // Set the BarData to the chart
-//        barChart.data = barData
-//        // Refresh the chart to display the updated data
-//        barChart.invalidate()
-//    }
-//}
-//
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -181,10 +64,18 @@ class CardManagement : Fragment() {
         barChart.setFitBars(true) // Make the bars fit the available space
         barChart.description.isEnabled = false // Disable the chart description
         barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM // Set the position of the x-axis
-        barChart.xAxis.setDrawLabels(true) // Enable the display of x-axis labels
-        barChart.xAxis.textColor = Color.BLACK // Set the color of the x-axis labels
-        barChart.xAxis.textSize = 12f // Set the text size of the x-axis labels
+        barChart.xAxis.setDrawLabels(false) // Enable the display of x-axis labels
         barChart.xAxis.granularity = 1f
+
+        //right y axis not shown
+        barChart.axisRight.isEnabled = false
+
+        //Customize the y Axis
+        val yAxisLeft = barChart.axisLeft
+        yAxisLeft.axisMinimum = 0f // Minimum value on Y-axis
+        yAxisLeft.granularity = 1f // Step size between axis values
+        yAxisLeft.textSize = 15f
+
 
         // Refresh the chart to display the data
         barChart.invalidate()
@@ -222,9 +113,14 @@ class CardManagement : Fragment() {
         val barData = BarData()
 
         // Create BarDataSet objects for each type of card.
-        val easyDataSet = createBarDataSet(entriesEasy, Color.GREEN, "Leicht")
-        val mediumDataSet = createBarDataSet(entriesMedium, Color.BLUE, "Mittel")
-        val hardDataSet = createBarDataSet(entriesHard, Color.RED, "Schwer")
+        val easyDataSet = createBarDataSet(entriesEasy, Color.rgb(51, 153, 51), "Leicht")
+        val mediumDataSet = createBarDataSet(entriesMedium, Color.rgb(51, 204, 204), "Mittel")
+        val hardDataSet = createBarDataSet(entriesHard, Color.rgb(204, 0, 0), "Schwer")
+
+        //change fontsize of the amount of cards per bar
+        easyDataSet.setValueTextSize(12f)
+        mediumDataSet.setValueTextSize(12f)
+        hardDataSet.setValueTextSize(12f)
 
         // Add the BarDataSet objects to BarData.
         barData.addDataSet(easyDataSet)
